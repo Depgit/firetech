b98c4dafb8c28183ab5410a7b50483079f46aa49
depgit <depcoder@gmail.com>
2022-03-19 17:34:45 +0530

    profile pic set

--- client/src/reducers/contest.js
+++ client/src/reducers/contest.js
@@ -1,8 +1,6 @@
 export const contestinitialState = []
 
 export const contestreducer = (contest, action)=>{
-    console.log("contest reducer page running .... ", contest,action);
-
     if(action.type==="CONTEST"){
         return [...contest, action.payload]
     }
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,8 +1,6 @@
 export const postinitialState = []
 
 export const postreducer = (post, action)=>{
-    console.log("Post reducer page running .... ", post,action);
-
     if(action.type==="POSTS"){
         return [...post, action.payload]
     }
